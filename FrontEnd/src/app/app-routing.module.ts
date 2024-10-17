be5d169f9867a82788a54ebd730c6f7c8b118cb5
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-10-17 11:48:04 -0300

    :motorway: feat: Adiciona rotas

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastroUserComponent } from './cadastro-user/cadastro-user.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { CadastroProdutosComponent } from './cadastro-produtos/cadastro-produtos.component';
+import { LancesComponent } from './lances/lances.component';
+import { AvaliacoesComponent } from './avaliacoes/avaliacoes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'cadastro-user', component: CadastroUserComponent },
+  { path: 'produtos', component: ProdutosComponent },
+  { path: 'cadastro-produtos', component: CadastroProdutosComponent },
+  { path: 'lances', component: LancesComponent },
+  { path: 'avaliacoes', component: AvaliacoesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
