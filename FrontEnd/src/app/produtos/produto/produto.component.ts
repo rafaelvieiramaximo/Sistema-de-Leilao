e9708f871e322b0d96ff2d95435c1d4925ec1ed8
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-10-19 15:28:45 -0300

    :sparkles: feat: Ajusta exibição produtos nos lances

--- FrontEnd/src/app/produtos/produto/produto.component.ts
+++ FrontEnd/src/app/produtos/produto/produto.component.ts
@@ -1,22 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Produto } from '../../services/produto.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-produto',
   templateUrl: './produto.component.html',
   styleUrls: ['./produto.component.css'],
 })
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
   @Input() produto!: Produto;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.produto = {
+        id_produto: +params['id_produto'] || this.produto.id_produto,
+        nome: params['nome'] || this.produto.nome,
+        descricao: params['descricao'] || this.produto.descricao,
+        preco_inicial: +params['preco'] || this.produto.preco_inicial,
+        data_inicial: params['data'] || this.produto.data_inicial,
+      };
+    });
+  }
 
   navAvaliacao() {
     this.router.navigate(['/avaliacoes', this.produto.id_produto]);
   }
 
-  navLances() {
-    this.router.navigate(['/lances', this.produto.id_produto]);
+  navLances(produtoLink: Produto) {
+    this.router.navigate([
+      '/lances',
+      produtoLink.data_inicial,
+      produtoLink.descricao,
+      produtoLink.id_produto,
+      produtoLink.nome,
+      produtoLink.preco_inicial,
+    ]);
   }
 }
