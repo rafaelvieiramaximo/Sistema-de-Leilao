1d38991b8c95c774ee001df72b6cd5ab330f14ce
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-10-20 20:51:55 -0300

    :wrench: feat: Adiciona sistema de edição

--- FrontEnd/src/app/usuarios/usuario/usuario.component.ts
+++ FrontEnd/src/app/usuarios/usuario/usuario.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Usuario, UsuarioService } from '../../services/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css'] // Corrigido: `styleUrl` para `styleUrls`
+  styleUrls: ['./usuario.component.css'], // Corrigido: `styleUrl` para `styleUrls`
 })
 export class UsuarioComponent {
   @Input() usuario!: Usuario;
 
-  constructor(private usuarioService: UsuarioService) {}
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
   // Método para deletar o usuário
   deleteUsuario() {
@@ -19,10 +20,20 @@ export class UsuarioComponent {
           console.log('Usuário deletado com sucesso.');
           // Aqui você pode adicionar código para remover o usuário da UI
         },
-        error => {
+        (error) => {
           console.error('Erro ao deletar o usuário:', error);
         }
       );
     }
+  }
+
+  editUsuario() {
+    this.router.navigate([
+      '/usuario',
+      this.usuario.email,
+      this.usuario.id_usuario,
+      this.usuario.nome,
+      this.usuario.reputacao,
+    ]);
   }
 }
