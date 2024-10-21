dc6b0fdaf11f1615e8052b025d1cd337df8d579b
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-10-20 21:54:44 -0300

    :lipstick: feat: Exibe avaliações

--- FrontEnd/src/app/avaliacoes/avaliacoes.component.ts
+++ FrontEnd/src/app/avaliacoes/avaliacoes.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Avaliacao, AvaliacaoService } from '../services/avaliacao.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-avaliacoes',
   templateUrl: './avaliacoes.component.html',
-  styleUrl: './avaliacoes.component.css'
+  styleUrl: './avaliacoes.component.css',
 })
 export class AvaliacoesComponent {
+  lancesProduto: Avaliacao[] = [];
 
+  constructor(
+    private avaliacaoServ: AvaliacaoService,
+    private actvRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.actvRoute.params.subscribe((params) => {
+      const produtoId = params['id_produto'];
+
+      this.avaliacaoServ.getAvaliacoesByProdutoId(produtoId).subscribe({
+        next: (avaliacoes: Avaliacao[]) => {
+          this.lancesProduto = avaliacoes;
+        },
+        error: (err) => {
+          console.error('Erro ao buscar avaliações:', err);
+        },
+      });
+    });
+  }
 }
