549cb3d99270d7282171c9a5bbe75b473a05d85b
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-10-20 23:22:28 -0300

    :wrench: feat: Adiciona adição de lances

--- FrontEnd/src/app/lances/lances.component.ts
+++ FrontEnd/src/app/lances/lances.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { Lance, LanceService } from '../services/lance.service';
+import { AvaliacaoService } from '../services/avaliacao.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lances',
   templateUrl: './lances.component.html',
-  styleUrl: './lances.component.css'
+  styleUrl: './lances.component.css',
 })
 export class LancesComponent {
+  lancesProduto: Lance[] = [];
+  id_user: number | null = null;
+  valor: number = 0;
 
+  dataAtual = new Date();
+  dia = String(this.dataAtual.getDate()).padStart(2, '0');
+  mes = String(this.dataAtual.getMonth() + 1).padStart(2, '0');
+  ano = this.dataAtual.getFullYear();
+  dataFormatada = `${this.ano}-${this.mes}-${this.dia}`;
+  produtoId: number = 0;
+
+  constructor(
+    private lanceSrv: LanceService,
+    private actvRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.actvRoute.params.subscribe((params) => {
+      this.produtoId = params['id_produto'];
+
+      this.lanceSrv.getLancesByProdutoId(this.produtoId).subscribe({
+        next: (lances: Lance[]) => {
+          this.lancesProduto = lances;
+        },
+        error: (err) => {
+          console.error('Erro ao buscar avaliações:', err);
+        },
+      });
+    });
+  }
+
+  cadastroAval() {
+    const novoLance = {
+      id_usuario: this.id_user !== null ? this.id_user : 0,
+      valor: this.valor,
+      data: this.dataFormatada,
+      id_produto: +this.produtoId,
+    };
+
+    this.lanceSrv.createLance(novoLance).subscribe({
+      next: (lanceCriado) => {
+        console.log('Lance criado com sucesso:', lanceCriado);
+      },
+      error: (err) => {
+        console.error('Erro ao criar avaliação:', err);
+      },
+    });
+  }
 }
--- FrontEnd/src/app/services/lance.service.ts
+++ FrontEnd/src/app/services/lance.service.ts
@@ -11,14 +11,23 @@ export interface Lance {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LanceService {
   private apiUrl = 'http://localhost:5000/lances';
+  private apiUrl2 = 'http://localhost:5000/lance';
 
   constructor(private http: HttpClient) {}
 
   getLances(): Observable<Lance[]> {
     return this.http.get<Lance[]>(this.apiUrl);
+  }
+
+  getLancesByProdutoId(id_produto: number): Observable<Lance[]> {
+    return this.http.get<Lance[]>(`${this.apiUrl2}/${id_produto}`);
+  }
+
+  createLance(lance: Partial<Lance>): Observable<Lance> {
+    return this.http.post<Lance>(`${this.apiUrl2}`, lance);
   }
 }
