37bb9863bf1330eca52c3e05068910964454ddd3
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-11-12 17:57:52 -0300

    :bug: fix : update payment model fields and enhance payment routes with CRUD operations

--- BackEnd/app/models/payment_model.py
+++ BackEnd/app/models/payment_model.py
@@ -1,12 +1,10 @@
-from mongoengine import Document, StringField, FloatField, IntField, ReferenceField, UUIDField
+from mongoengine import Document, StringField, FloatField, IntField, ReferenceField
 from app.models.users_model import Usuario_Model
 from app.models.product_model import Produto_Model
-import uuid
 #Modulação da Tabela Pagamentos
 class Pagamento_Model(Document):
-    id_pagamento = UUIDField(primary_key=True, default=uuid.uuid4)
-    usuario = ReferenceField(Usuario_Model, required=True)
-    produto = ReferenceField(Produto_Model, required=True)
+    id_usuario = ReferenceField(Usuario_Model, required=True)
+    id_produto = ReferenceField(Produto_Model, required=True)
     valor_total = FloatField(required=True)
     forma_pagamento = StringField(max_length=255, required=True)
     status = StringField(max_length=255, required=True)
