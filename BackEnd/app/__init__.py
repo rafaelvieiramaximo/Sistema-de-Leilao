e0bc7890d08c38327ecc4b85520e78bf80c1936e
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-11-14 16:22:22 -0300

    :sparkles: feat: Community enabled

--- BackEnd/app/__init__.py
+++ BackEnd/app/__init__.py
@@ -7,6 +7,7 @@ from app.routers.users_routes import Users, User
 from app.routers.product_routes import Produto, Produtos
 from app.routers.payment_routes import Pagamentos, Pagamento
 from app.routers.freight_routes import Fretes, Frete
+from app.routers.community_routes import Comunidades, Comunidade
 from app.db import db
 
 import os
@@ -37,7 +38,9 @@ def create_app():
     api.add_resource(Pagamento, '/pagamento', '/pagamento/<string:id_pagamento>')
     api.add_resource(Fretes, '/fretes')
     api.add_resource(Frete, '/frete', '/frete/<string:cte>')
-    
+    api.add_resource(Comunidades, '/comunidades'),
+    api.add_resource(Comunidade, '/comunidade', '/comunidade/<string:id_comunidade>')
+
     #app.register_blueprint(routes)
 
     return app
