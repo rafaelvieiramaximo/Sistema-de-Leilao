de2fd14635f7977d292500439686bef382150267
Rafael <rafavmaximo@gmail.com>
2024-11-18 10:08:30 -0300

    :bug: fix: Remove redundant primary key from Usuario_Model

--- BackEnd/app/models/users_model.py
+++ BackEnd/app/models/users_model.py
@@ -2,7 +2,6 @@ from mongoengine import Document, StringField, FloatField, ObjectIdField
 
 # Modulação da Tabela Usuario
 class Usuario_Model(Document):
-    id_usuario = ObjectIdField(primary_key=True) 
     nome = StringField(required=True)
     email = StringField(required=True)
     senha = StringField(required=True)
